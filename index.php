364d1fb081b39ec74aa399a839135e7e1689c0c1
whodeveloper <whodevelopthis@gmail.com>
2015-11-17 10:59:58 +0800

    racking v1.0

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+
+<html>
+
+	<head>
+		<title>Wong Lye Enterpise</title>
+		<link rel = "stylesheet" type = "text/css" href = "css/style1.css">
+	</head>
+
+	<body>
+
+		<div class = "fullwrap">
+
+			<div class = "header">
+				<h1> Wong Lye Enterprise</h1>
+			</div>
+
+			<div class = "navbar">
+				<ul>
+					<ls>Home</ls>
+					<ls>About</ls>
+					<ls>Contact</ls>
+				</ul>
+			</div>
+
+			<div class = "maincontent">
+				
+			</div>
+
+			<div class = "footer">
+				
+			</div>
+
+		</div>
+
+	</body>
+
+</html>
+
+
